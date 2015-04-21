3b66690e8e761357e4145ef061e42a6b5367ff2f
linuxxxxx <joey@cyanogenmoditalia.it>
2015-04-21 20:43:01 +0200

    [2/2] Dialer: SmartMute
    Mute the incoming call by flipping the phone
    
    Change-Id: I2009bc7b4d0d1b23f76b232e156ea0fa35e3ad5b
    Signed-off-by: linuxxxxx <joey@cyanogenmoditalia.it>
    
    Conflicts:
    	res/values/cm_strings.xml
    	res/xml/general_settings.xml
    	src/com/android/dialer/settings/GeneralSettingsFragment.java

--- src/com/android/dialer/settings/GeneralSettingsFragment.java
+++ src/com/android/dialer/settings/GeneralSettingsFragment.java
@@ -49,6 +49,8 @@ public class GeneralSettingsFragment extends PreferenceFragment
     private static final String BUTTON_VIBRATE_ON_RING = "button_vibrate_on_ring";
     private static final String BUTTON_PLAY_DTMF_TONE  = "button_play_dtmf_tone";
     private static final String BUTTON_RESPOND_VIA_SMS_KEY = "button_respond_via_sms_key";
+    public static final String BUTTON_SMART_DIALER_KEY = "button_smart_dialer";
+    public static final String BUTTON_SMART_MUTE_KEY = "button_smart_mute";
 
     private static final int MSG_UPDATE_RINGTONE_SUMMARY = 1;
 
